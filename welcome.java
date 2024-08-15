6f79a0f78b4fba7513954cc1a4fb37d071f439a0
NguyenVantien03 <22002395.tien@student.iuh.edu.vn>
2024-08-16 00:33:40 +0700

    firstprj

---
+++ welcome.java
@@ -0,0 +1,7 @@
+ public class welcome {
+      public static void main(String[] args) {
+          System.out.println("hello nguye van tien!");
+          System.out.println("welcome to the DHHTTT19B!");
+      }
+}
+
